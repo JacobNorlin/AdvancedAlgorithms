ff78a656d1673ac58d39ffcc7f18ce62d94750e3
Rogier Slag <rogier.slag@gmail.com>
2014-02-01 14:15:06 +0100

    Added support to several graph subtypes

--- src/vertexcover/Graph.java
+++ src/vertexcover/Graph.java
@@ -1,6 +1,7 @@
 package vertexcover;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Graph {
 
@@ -8,85 +9,81 @@ public class Graph {
 	private ArrayList<Edge> edges;
 	private int minCover;
 
-	public Graph(){
+	public Graph() {
 		nodes = new ArrayList<Node>();
 		edges = new ArrayList<Edge>();
 	}
-
 
 	public ArrayList<Edge> getEdges() {
 		return edges;
 	}
 
-
 	public ArrayList<Node> getNodes() {
 		return nodes;
 	}
 
-
-
-	public void addNode(Node n){
+	public void addNode(Node n) {
 		nodes.add(n);
 		for (Edge e : n.getEdges()) {
-			if(!edges.contains(e))
+			if (!edges.contains(e))
 				edges.add(e);
 		}
 	}
 
-	public void addEdge(Edge e){
-		//this is dumb i dont like this
+	public void addEdge(Edge e) {
+		// this is dumb i dont like this
 		nodes.get(nodes.indexOf(e.getU())).addEdge(e);
 		nodes.get(nodes.indexOf(e.getV())).addEdge(e);
 		edges.add(e);
 	}
 
 	@Override
-	public String toString(){
+	public String toString() {
 
 		StringBuilder sb = new StringBuilder();
-		for(Edge e : edges){
+
+		for (Node n : nodes) {
+			sb.append(n);
+			sb.append("\n");
+		}
+		sb.append("\n");
+
+		for (Edge e : edges) {
 			sb.append(e);
 			sb.append("\n");
 		}
 		return sb.toString();
 
-
 	}
 
-	public int getAmountOfEdges(){
+	public int getAmountOfEdges() {
 		return edges.size();
 	}
 
-	public void removeNode(Node n){
-//		System.out.println(n);
-//		System.out.println(n.getEdges());
-		for(Edge e : n.getEdges()){
+	public void removeNode(Node n) {
+		// System.out.println(n);
+		// System.out.println(n.getEdges());
+		for (Edge e : n.getEdges()) {
 			int indexOfEdge = edges.indexOf(e);
-			if(indexOfEdge >= 0){
+			if (indexOfEdge >= 0) {
 				edges.remove(indexOfEdge);
-//				System.out.print(e+" ");
+				// System.out.print(e+" ");
 			}
-
-
 
 		}
 		nodes.remove(nodes.indexOf(n));
 	}
 
-	public Edge getRandomEdge(){
-		return edges.get(GraphFactory.randInt(0, edges.size()-1));
+	public Edge getRandomEdge() {
+		return edges.get(GraphFactory.randInt(new Random(), edges.size() - 1));
 	}
-
 
 	public int getMinCover() {
 		return minCover;
 	}
 
-
 	public void setMinCover(int minCover) {
 		this.minCover = minCover;
 	}
-
-
 
 }
--- src/vertexcover/GraphFactory.java
+++ src/vertexcover/GraphFactory.java
@@ -8,73 +8,137 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class GraphFactory {
-	
-	public static Graph buildRandomGraph(int nodes, int edges){
-		
+
+	public static Graph buildRandomGraph(int nodes, int edges) {
+
 		Graph newGraph = new Graph();
-		ArrayList<Node> nodeList = new ArrayList<Node>();
-		
-		for(int i = 0; i < nodes; i++){
+		Random r = new Random();
+
+		for (int i = 0; i < nodes; i++) {
 			Node n = new Node(i);
 			newGraph.addNode(n);
 		}
-		
-		while(edges != 0){
-			Node u = newGraph.getNodes().get(randInt(0,nodes-1));
-			Node v = newGraph.getNodes().get(randInt(0,nodes-1));
-			
-			if(u.equals(v)){
+
+		while (edges != 0) {
+			int i = randInt(r, nodes - 1);
+			Node u = newGraph.getNodes().get(i);
+			Node v = newGraph.getNodes().get(randInt(r, nodes - 1, i));
+
+			if (u.equals(v)) {
 				continue;
 			}
-			
-			Edge e = new Edge(u,v);
+
+			Edge e = new Edge(u, v);
 			newGraph.addEdge(e);
 			edges--;
-			
-			
+
 		}
-		
+
 		return newGraph;
 	}
-	
-	
-	public static int randInt(int min, int max) {
 
-	    // Usually this can be a field rather than a method variable
-	    Random rand = new Random();
+	public static Graph buildNearCompleteGraph(int nodes, double skip) {
 
-	    // nextInt is normally exclusive of the top value,
-	    // so add 1 to make it inclusive
-	    int randomNum = rand.nextInt((max - min) + 1) + min;
+		Graph newGraph = new Graph();
+		Random r = new Random();
 
-	    return randomNum;
+		for (int i = 0; i < nodes; i++) {
+			Node n = new Node(i);
+			newGraph.addNode(n);
+		}
+
+		int randVal = new Double(skip*100).intValue() - 1;
+
+		for (int i = 0; i < nodes; i++) {
+			for (int j = i; j < nodes; j++) {
+				if (i == j || r.nextInt(100) < randVal) {
+					continue;
+				}
+				Node u = newGraph.getNodes().get(i);
+				Node v = newGraph.getNodes().get(j);
+
+				Edge e = new Edge(u, v);
+				newGraph.addEdge(e);
+			}
+		}
+
+		return newGraph;
 	}
-	
-	public static Graph buildGraphFromFile(String path) throws IOException{
-		 BufferedReader br = new BufferedReader(new FileReader(path));
-		 Graph newGraph = new Graph();
-		    try {
-		        String[] firstLine = br.readLine().split(" ");
-//		        newGraph.setMinCover(Integer.parseInt(firstLine[2]));
-		        int edges = Integer.parseInt(firstLine[3]);
-		        int nodes = Integer.parseInt(firstLine[2]);
-		        for(int i = 1; i <= nodes; i++){
-		        	Node n = new Node(i);
-		        	newGraph.addNode(n);
-		        }
-		        
-		        String[] line;
-		        for(int i = 0; i<edges; i++){
-		        	line = br.readLine().split(" ");
-		        	Node u = new Node(Integer.parseInt(line[1]));
-		        	Node v = new Node(Integer.parseInt(line[2]));
-		            Edge newEdge = new Edge(u, v);
-		            newGraph.addEdge(newEdge);
-		        }
-		    } finally {
-		        br.close();
-		    }
-		    return newGraph;
+
+	public static Graph buildNearSparseGraph(int nodes, double skip) {
+
+		Graph newGraph = new Graph();
+		Random r = new Random();
+
+		for (int i = 0; i < nodes; i++) {
+			Node n = new Node(i);
+			newGraph.addNode(n);
+		}
+
+		int randVal = new Double(skip*100).intValue() - 1;
+
+		for (int i = 0; i < nodes; i++) {
+			for (int j = i; j < nodes; j++) {
+				if (i == j || r.nextInt(100) > randVal) {
+					continue;
+				}
+				Node u = newGraph.getNodes().get(i);
+				Node v = newGraph.getNodes().get(j);
+
+				Edge e = new Edge(u, v);
+				newGraph.addEdge(e);
+			}
+		}
+
+		return newGraph;
+	}
+
+	public static int randInt(Random r, int max) {
+		return randInt(r, max, -1);
+	}
+
+	public static int randInt(Random r, int max, int exclude) {
+
+		// nextInt is normally exclusive of the top value,
+		// so add 1 to make it inclusive
+		int randomNum;
+		while ((randomNum = r.nextInt(max + 1)) != exclude)
+			;
+
+		return randomNum;
+	}
+
+	public static Graph buildGraphFromFile(String path) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		Graph newGraph = new Graph();
+		try {
+			String[] firstLine = br.readLine().split(" ");
+			// newGraph.setMinCover(Integer.parseInt(firstLine[2]));
+			int edges = Integer.parseInt(firstLine[3]);
+			int nodes = Integer.parseInt(firstLine[2]);
+			for (int i = 1; i <= nodes; i++) {
+				Node n = new Node(i);
+				newGraph.addNode(n);
+			}
+
+			String[] line;
+			for (int i = 0; i < edges; i++) {
+				line = br.readLine().split(" ");
+				Node u = new Node(Integer.parseInt(line[1]));
+				Node v = new Node(Integer.parseInt(line[2]));
+				Edge newEdge = new Edge(u, v);
+				newGraph.addEdge(newEdge);
+			}
+		} finally {
+			br.close();
+		}
+		return newGraph;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(buildNearCompleteGraph(3, 0.1));
+		System.out.println(buildNearSparseGraph(3, 0.1));
 	}
 
 }
